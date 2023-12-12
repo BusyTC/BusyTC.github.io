60ea6c2073b8064964215a973195fdf9592d62d1
BusyTC <61108485+BusyTC@users.noreply.github.com>
2023-12-13 00:05:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+for (let button of document.getElementsByClassName("like")) {
+    button.onclick = function () {
+        if (button.style.background === "ghostwhite" || !button.style.background) {
+            button.style.background = "#DE3163";
+            button.className = "like liked";
+        }
+        else {
+            button.style.background = "ghostwhite";
+            button.className = "like";
+        }
+    }
+}
+
+
+let images = [];
+for (let image of document.getElementsByClassName("img_animation_text_over")) {
+    images.push(image);
+}
+document.getElementById("show-liked").onclick = function () {
+    let button = document.getElementById("show-liked");
+    let gallery = document.getElementById("gallery");
+    gallery.innerHTML = "";
+    if (button.textContent.endsWith("понравившиеся")) {
+        button.textContent = "Показать все";
+        for (let image of images) {
+            if (image.querySelectorAll(".img_description button")[0].className === "like liked") {
+                gallery.appendChild(image);
+            }
+        }
+    }
+    else {
+        button.textContent = "Показать понравившиеся";
+        gallery.innerHTML = "";
+        for (let image of images) {
+            gallery.appendChild(image);
+        }
+    }
+}
